d6428edfe191aacd1c074539b0618116129a41c6
Ahmet AKDEMIR <ahmet.akdemir@st.uskudar.edu.tr>
2023-03-29 07:59:43 +0300

    --

--- DataAccess/Concrete/InMemory/InMemoryCarDal.cs
+++ DataAccess/Concrete/InMemory/InMemoryCarDal.cs
@@ -10,24 +10,48 @@ namespace DataAccess.Concrete.InMemory
 {
     public class InMemoryCarDal : ICarDal
     {
+        List<Car> _cars;
+        public InMemoryCarDal()
+        {
+            _cars = new List<Car>
+            {
+                new Car(){BrandId = 1,CarId = 1,ColorId = 1,DailyPrice = 120000,ModelYear = 2011,Description = "Temiz"},
+                new Car(){BrandId = 1,CarId = 2,ColorId = 2,DailyPrice = 140000,ModelYear = 2016,Description = "Ağır hasarlı"},
+                new Car(){BrandId = 4,CarId = 3,ColorId = 3,DailyPrice = 180000,ModelYear = 2015,Description = "Temiz"}
+
+            };
+        }
         public void Add(Car car)
         {
-            throw new NotImplementedException();
+            _cars.Add(car);
         }
 
         public void Delete(Car car)
         {
-            throw new NotImplementedException();
+            Car CarToDelete;
+            CarToDelete = _cars.SingleOrDefault(c => c.CarId == car.CarId);
+            _cars.Remove(CarToDelete);
         }
 
         public List<Car> GetAll()
         {
-            throw new NotImplementedException();
+            return _cars;
+        }
+
+        public List<Car> GetAllByBrandId(int BrandId)
+        {
+           return _cars.Where(c=>c.BrandId == BrandId).ToList();
         }
 
         public void Update(Car car)
         {
-            throw new NotImplementedException();
+            Car CarToUpdate;
+            CarToUpdate = _cars.SingleOrDefault(c => c.CarId == car.CarId);
+            CarToUpdate.ColorId = car.ColorId;
+            CarToUpdate.DailyPrice = car.DailyPrice;
+            CarToUpdate.ModelYear = car.ModelYear;
+            CarToUpdate.Description = car.Description;
+            CarToUpdate.BrandId = car.BrandId;
         }
     }
 }
